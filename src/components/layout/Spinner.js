3960b93d82f7e300a31957205854f417b5ba2627
kaustubhtalathi <kaustubhtalathi@gmail.com>
2022-12-30 20:52:50 -0800

    init

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import { FaSpinner } from "react-icons/fa";
+
+function Spinner() {
+    return (
+        <div className="w-100 mt-20">
+            <FaSpinner width="180" />
+        </div>
+    );
+}
+
+export default Spinner;
---
+++ src/components/users/UserResults.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+
+import GithubContext from "../../context/github/GithubContext";
+
+function UserResults() {
+    const { users, isLoading } = useContext(GithubContext);
+
+    if (isLoading) {
+        return <Spinner />;
+    } else {
+        return (
+            <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grids-cols-3 md:grid-cols-2">
+                {users.map((user) => {
+                    return <UserItem key={user.login} user={user} />;
+                })}
+            </div>
+        );
+    }
+}
+
+export default UserResults;
